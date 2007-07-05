14c58123fbe8c5f4a16a887520800b4ad6fa6675
Morgan Jones <morgan@morganjones.org>
2007-07-05 14:56:29 +0000

    working, no headers, prints columns as-is, doesn't check for email address in multiple columns

--- jes/pab2csv.pl
+++ jes/pab2csv.pl
@@ -5,8 +5,10 @@
 #
 # Convert a Java Messaging Server 6.2 PAB to CSV
 #
-#  ~/Docs/utils/trunk/jes/pab2csv.pl -u ~/Docs/ou/pab2csv/dc_ou_dc_edu_070522.ldif -p ~/Docs/ou/pab2csv/pab_no_mime.ldif -o contacts -a '"" givenname "" sn "" "" "" "" street "" "" l st postalcode co "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" facsimileTelephoneNumber telephoneNumber "" "" "" "" "" homephone "" "" mobile "" "" pager "" "" "" "" "" "" "" dateofbirth "" "" "" "" mail "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" labeleduri'|more
+# cd working/dir/path
+#  ~/Docs/utils/trunk/jes/pab2csv.pl -u dc_ou_dc_edu_070522.ldif -p pab_no_mime.ldif -o contacts -a '"" givenname "" sn "" "" "" "" street "" "" l st postalcode co "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" facsimileTelephoneNumber telephoneNumber "" "" "" "" "" homephone "" "" mobile "" "" pager "" "" "" "" "" "" "" dateofbirth "" "" "" "" mail "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" labeleduri'|more
 #
+# ~/Docs/utils/trunk/jes/pab2csv.pl -u dc_ou_dc_edu_070522.ldif  -p pab_no_mime.ldif -o contacts 'givenname sn cn mail telephonenumber homephone mobile pager facsimiletelephonenumber address l st postalcode co labeleduri dateofbirth description'
 use strict;
 use Getopt::Std;
 
@@ -32,7 +34,8 @@ my $csv_out_dir = $opts->{o} || print_usage();
 
 print "attr_list: /$user_attr_list/\n";
 
-my @pab_attrs_to_collect = split(' ', $user_attr_list);
+my @pab_attrs_to_collect = split(/\s{1}/, $user_attr_list);
+
 
 #open (OUT, ">$csv_out") || die "can't open $csv_out for writing";
 
@@ -46,9 +49,9 @@ open(PAB, $pab_ldif) || die "can't open $pab_ldif";
 my $contacts;
 while (my $a = get_next_contact($pab2uid_h)) {
     my ($uid, @contact) = @$a;
-    # desired output format here.
-    $opts->{d} && print "$uid, contact: " . join(', ', @contact) . "\n";
-    $opts->{d} && print "\n\n\n";
+    if ($uid eq "jone7099") {
+	$opts->{d} && print "$uid, contact: " . join(', ', @contact) . "\n";
+    }
     #print "$uid," . join(',', @contact) . "\n";
     # this won't scale.
     push @{$contacts->{$uid}}, join(',', @contact);
@@ -58,10 +61,8 @@ close(PAB);
 print "\nwriting csv files..\n";
 
 for my $u (sort keys %$contacts) {
-    print "writing csv for $u..\n";
 
     my $outfile = "$csv_out_dir/$u.csv";
-    print "outfile: $outfile\n";
     if (!open (OUT, ">$outfile")) { 
 	print "can't open $outfile";
 	next;
@@ -139,14 +140,25 @@ sub parse_pab_entry($$) {
 
     my $dn;
     my @r;
-    if ($e =~ /dn:\s*([^\n]+)\n/i) { 
+    # ignore entries that don't contain a dn and 'un=AddressBook' entries
+    if ($e =~ /dn:\s*([^\n]+)\n/i && $e !~ /dn:\s*un=addressbook/i) { 
         $dn = $1; 
-    } else {
+    }	else {
         return @r;    
     }
-    
+
+    # strip off the first item in the dn:
+    #  dn:  un=MorganJones4df9d55,ou=uniqueIdentifier=9120,ou=people,o=ou.edu,dc=ou,dc=edu,o=pab
+    #  becomes ou=uniqueIdentifier=9120,ou=people,o=ou.edu,dc=ou,dc=edu,o=pab
+    #
     $e =~ /dn:\s*[^\,]+,\s*([^\n]+)\n/i;
     my $pt = $1;
+    if ($e =~ /128837/) {
+	print "pt: /$pt/\n";
+	if (!exists $p2u->{lc $pt}) {
+	    print "not in p2u..\n";
+	}
+    }
     #$pt = (split /\,/, $dn)[0];
     #$pt =~ s/dn:\s*//i;
 
@@ -154,12 +166,24 @@ sub parse_pab_entry($$) {
         #$opts->{d} && print "orphaned pab tree or container: $dn\n";
     } else { 
         my $u =  $p2u->{lc $pt};
+	if ($u eq "jone7099") {
+	    print "pushing $u\n";
+	    print "e: /$e/\n";
+	}
         push @r, $u;
 
         # pull the attributes out of the entry:
         for my $a (@pab_attrs_to_collect) {
-            if ($a !~ /^\s*$/ && $e =~ /$a:\s*([^\n]+)\n/) {
+	    
+	    if ($u eq "jone7099") {
+		print "$a..\n";
+	    }
+
+            if ($a !~ /^\s*$/ && $e =~ /\n$a:\s*([^\n]+)\n/i) {
                 my $v = $1;
+		if ($u eq "jone7099") {
+		    print "$a: $v\n";
+		}
                 push @r, $v;
             } else {
                 push @r, '';
@@ -188,18 +212,19 @@ sub get_pab_uris($) {
 
         # Ignore the entry if it does not have uid & paburi attributes.
         if (defined $uid && defined $paburi) {
-            #$opts->{d} && print "$uid: $paburi\n";
-	    # strip off 'ldap://host.domain.com:'
+
+	    # strip off 'ldap://host.domain.com:port'
+	    #
             # ldap://pabldap.ou.edu:389/ou=uniqueIdentifier=12436,ou=people,o=ou.edu,dc=ou,dc=edu,o=pab
             # becomes ou=uniqueIdentifier=12436,ou=people,o=ou.edu,dc=ou,dc=edu,o=pab
-	    # we don't want them, we read from a text dump of the pab
             $paburi =~ s/ldap\:\/\/[^\/]+\///i;
+
             if (exists $p2u_h->{lc $paburi} && 
                 lc $uid eq lc $p2u_h->{lc $paburi}){
                 warn("$paburi already in hash.  Was $p2u_h->{lc $paburi}, ".
                     "now $uid");
             } else {
-		#print "adding $uid: /$paburi/\n";
+		$opts->{d} && print "adding $uid: /$paburi/\n";
                 $p2u_h->{lc $paburi} = $uid;
             }
         }
@@ -225,7 +250,8 @@ sub print_usage() {
     print "\texport ldif with db2ldif:\n";
     print "\t./db2ldif -U1Nu -s dc=domain,dc=com\n".
           "\t\t-a /tmp/dc_domain_dc_com.ldif\n";
-    print "\t./db2ldif -U1Nu -s o=pab -a /tmp/o_pab.ldif\n";
+    #print "\t./db2ldif -U1Nu -s o=pab -a /tmp/o_pab.ldif\n";
+    print "\t./db2ldif -UNm1u -s o=pab -a /tmp/o_pab.ldif\n";
     print "\n";
     print "\tthen $0 -u /tmp/dc_domain_dc_com.ldif -p /tmp/o_pab.ldif\n".
           "\t\t-o contacts\n";
