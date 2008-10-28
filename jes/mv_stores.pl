b369bcc83de13df87e531f2932f1ce05b9a46144
Morgan Jones <morgan@morganjones.org>
2008-10-28 14:15:53 +0000

    script to move user stores to correct locations based on ldap entry originally built for a site moving from one domain with aliases to hosted domains. TODO: needs to be made site independent

---
+++ jes/mv_stores.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl -w
+#
+
+use strict;
+
+my @domain_from =    "ext.domain.org";
+my @hosted_domains = qw/srdc.domain.org cett.dc=domain,dc=org create4ms.dc=domain,dc=org davfm.dc=domain,dc=org/;
+
+for my $d (@hosted_domains) {
+    print "\n$d:\n";
+
+    for my $u (`/usr/bin/ldapsearch -D "cn=directory manager" -w 'pass' -Lb o=$d,o=msu_ag objectclass=inetmailuser uid|egrep '^uid'|cut -d ' ' -f 2`) {
+        chomp $u;
+        print "$u\n";
+        my $mboxutil = `/opt/SUNWmsgsr/sbin/mboxutil -l`;
+        my @m = grep (/user\/($u\@[^\/]+|$u)\/INBOX/, split(/\n/, $mboxutil));
+        print join ("\n", @m), "\n";;
+
+        # if both user and user@$d exist mboxtuil -d then mboxutil -r user user@
+        # if just user exists mboxutil -r the user to '@'
+        # if just user@ do nothing
+        # print "u: /$u/ d: /$d/\n";
+        if ((my ($usra) = grep /(user\/[^\@]+\@$d\/INBOX)/, @m) &&
+            (my ($usr) =  grep /(user\/$u\/INBOX)/, @m)) {
+            $_ = $usra;
+            ($usra) = /(user\/[^\@]+\@$d\/INBOX)/;
+            $_ = $usr;
+            ($usr)  = /(user\/$u\/INBOX)/;
+            my $d_cmd = "/opt/SUNWmsgsr/sbin/mboxutil -d $usra";
+            print "$d_cmd\n";
+            system "$d_cmd";
+            my $r_cmd =  "/opt/SUNWmsgsr/sbin/mboxutil -r $usr $usra\n";
+            print "$r_cmd\n";
+            system "$r_cmd";
+        } elsif (($usr) = grep /(user\/$u\/INBOX)/, @m) {
+            $_ = $usr;
+            ($usr) = /(user\/$u\/INBOX)/;
+            my $usra = $usr;
+            ($usra) =~ s/$u/$u\@$d/;
+            my $r_cmd = "/opt/SUNWmsgsr/sbin/mboxutil -r $usr $usra"; 
+            print "$r_cmd\n";
+            system "$r_cmd";
+        }
+        print "\n";
+    }
+} 
