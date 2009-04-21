bec0d42161b4ed919fb29165a6714c8ee6d046f9
Morgan Jones <morgan@morganjones.org>
2009-04-21 15:15:57 +0000

    version used in sdp production migration

--- mail/run_migrate.sh
+++ mail/run_migrate.sh
@@ -1,30 +1,49 @@
-#!/usr/bin/sh
+#!/bin/sh
 #
+# Though designed to be general purpose this is currently set up for
+# Zimbra's ldap.  It wouldn't be hard to generalize or customize for a
+# different ldap.
 
+# directory containing utilities this script will use.. notably
+# mbox_migrate.pl
 mig_dir="/home/morgan/zimbra_migration"
-if [ -z "$1" ]; then
-    var_mail="/var/mail2/morgans_s"
-else
-    var_mail="/var/mail2/${1}"
-fi
 
+# location of mbox files.  Usually /var/mail
+var_mail="/var/mail2/morgan_combined"
+
+# zimbra ldap admin password
 z_admin_pass="pass"
+# password all user accounts are set to
 common_user_pass="pass"
+
+if [ ! -z "$1" ]; then
+    echo "1: $1";
+fi
 
 
 echo "starting migration at `date`"
 echo
-echo "migrating from ${var_mail}"
+if [ -z "$1" ]; then
+    echo "migrating $1 from ${var_mail}"
+else
+    echo "migrating from ${var_mail}"
+fi
 
-for user in `ls -1 ${var_mail}`; do 
-    # lookup the user's store to bypass the proxy.
-    #   This may not be strictly necessary anymore..
-    host=`/opt/csw/bin/ldapsearch -xLL -w "${z_admin_pass}" -h dmldap01.domain.org -D cn=config -Lb dc=domain,dc=org  uid=$user zimbramailhost|grep -i zimbramailhost|awk '{print $2}'`
+for l in `echo $1|perl -e "while (<>) {print join (' ', split /,/);}"`; do
+    echo ${l}
 
-    echo
-    echo ${mig_dir}/mbox_migrate.pl  -m ${var_mail}/$user -h $host -u $user -w "${common_user_pass}" 
-    ${mig_dir}/mbox_migrate.pl  -m ${var_mail}/$user -h $host -u $user -w "${common_user_pass}" 
+    for path in `ls -1 ${var_mail}/${l}*`; do
+        # lookup the user's store to bypass the proxy.
+        #   This may not be strictly necessary anymore..
 
+	user=`basename $path`;
+
+        host=`/opt/csw/bin/ldapsearch -xLL -w "${z_admin_pass}" -h dmldap01.domain.org -D cn=config -Lb dc=domain,dc=org  uid=$user zimbramailhost|grep -i zimbramailhost|awk '{print $2}'`
+
+        echo
+        echo ${mig_dir}/mbox_migrate.pl  -m $path -h $host -u $user -w "${common_user_pass}" 
+        ${mig_dir}/mbox_migrate.pl  -m $path -h $host -u $user -w "${common_user_pass}" 
+    done
 done
 
 echo
