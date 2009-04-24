7bf0beea8dfd3adb2383196ccbf2b35f2d8a0820
Morgan Jones <morgan@morganjones.org>
2009-04-24 16:40:21 +0000

    updates for mailapp.philasd.org

--- zimbra/run_ldap2zimbra.sh
+++ zimbra/run_ldap2zimbra.sh
@@ -3,12 +3,17 @@
 # Morgan Jones (morgan@morganjones.org)
 # $Id$
 
-bin_path=/usr/local/sbin
-log_path=/home/ldap2zimbra
+bin_path=ldap2zimbra
+log_path=ldap2zimbra/log
 log=${log_path}/ldap2zimbra_`date +%y%m%d.%H:%M:%S`
 mail_to=ldap-admin@domain.org
 
-# ${bin_path}/ldap2zimbra.pl > ${log} 2>&1
-l2z_cmd="${bin_path}/ldap2zimbra.pl -e $*"
-$l2z_cmd > ${log} 2>&1
-mail -s "ldap2zimbra output" ${mail_to} < ${log}
+# l2z_cmd="${bin_path}/ldap2zimbra.pl -e $*"
+l2z_cmd="${bin_path}/ldap2zimbra.pl -n -e $*"
+# $l2z_cmd > ${log} 2>&1
+# mail -s "ldap2zimbra output" ${mail_to} < ${log}
+echo "** output logged to ${log}"
+echo
+echo $l2z_cmd
+$l2z_cmd 2>&1 | tee $log
+
