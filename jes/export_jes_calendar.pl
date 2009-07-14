1ff642c2c22a72b7b6993c86a1c0efe85a6d104f
Morgan Jones <morgan@morganjones.org>
2009-07-14 14:06:43 +0000

    many changes, this is essentially a v1.0.  It is the version used in St. Joe's '09 migration

--- jes/export_jes_calendar.pl
+++ jes/export_jes_calendar.pl
@@ -3,6 +3,7 @@
 # export_jes_calendar.pl
 # Morgan Jones (morgan@morganjones.org)
 # $Id$
+#
 # parse the output of cscal list and create a .ics calendar for every calendar
 #
 use strict;
@@ -13,24 +14,33 @@ sub print_usage();
 
 #########
 ### Site-specific settings
-my $cal_bin="/bellatrixdg00/luminis/products/SUNWics5/cal/sbin";
+# specific luminis install
+# my $cal_bin="/bellatrixdg00/luminis/products/SUNWics5/cal/sbin";
+my $cal_bin="/var/opt/SUNWics5/cal/sbin";
 my $cal_cmd="./cscal list";
 ### end site-specific settings
 
 
 
 my $opts;
-getopts('hu:o:p:f:d', \%$opts);
+getopts('hu:o:p:f:dac', \%$opts);
 
 $opts->{h} && print_usage();
 my $user_list = $opts->{u};
 my $pidm_map_file = $opts->{p} || print_usage();
 my $ics_out_dir = $opts->{o} || print_usage();
 
-if (exists $opts->{u} && exists $opts->{f}) {
-    print "-f and -u are mutually exclusive\n";
+if ((exists $opts->{u} && exists $opts->{f}) ||
+    (exists $opts->{u} && exists $opts->{a}) ||
+    (exists $opts->{a} && exists $opts->{f})) {
+
+    print "-f, -u and -a are mutually exclusive\n";
     print_usage();
 }
+
+print "-c used, only primary calendars will be exported\n"
+    if (exists $opts->{c});
+
 
 my @users2import;
 if (exists $opts->{f}) {
@@ -43,28 +53,35 @@ if (exists $opts->{f}) {
 } elsif (exists $opts->{u}) {
     @users2import = split /\s+/, $opts->{u};
         #if (exists $opts->{u});
-} else {
+} elsif (!exists $opts->{a}) {
     print_usage();
 }
 
 die "$ics_out_dir does not exist or is not writable"
     unless (-d $ics_out_dir);
 
-print "\nusers to import: ", join (' ', @users2import), "\n";
+if (!exists $opts->{a}) {
+    print "\nusers to export: ", join (' ', @users2import), "\n";
+} else {
+    print "exporting all users and calendars\n";
+}
+
 
 my %pidm_map;
 
-open (IN, $pidm_map_file) || die "can't open $pidm_map_file";
+if (exists $opts->{p}) {
+    open (IN, $pidm_map_file) || die "can't open $pidm_map_file";
 
-while (<IN>) {
-    chomp;
+    while (<IN>) {
+        chomp;
 
-    my ($pidm, $uid) = split /\,/;
-    $uid =~ s/\"//g;
-    if (exists $pidm_map{$pidm}) {
-        print "$pidm found more than once!?\n";
-    } else {
-        $pidm_map{$pidm} = $uid;
+        my ($pidm, $uid) = split /\,/;
+        $uid =~ s/\"//g;
+        if (exists $pidm_map{$pidm}) {
+            print "$pidm found more than once!?\n";
+        } else {
+            $pidm_map{$pidm} = $uid;
+        }
     }
 }
 
@@ -75,7 +92,7 @@ while (<EXEC>) {
     chomp;
 
     my ($name, $pidm, $status);
-    if (/[^:]+:([^:]+):\s+owner=([^\s]+)\s+status=(.*)/) {
+    if (/[^:]+:([^:]*):\s+owner=([^\s]+)\s+status=(.*)/) {
          $name = $1;
          $pidm = $2;
          $status = $3;
@@ -84,7 +101,7 @@ while (<EXEC>) {
          $status = $2;
     } else {
         print "ignoring unparsable entry: /$_/\n"
-            if (exists $opts->{d};
+            if (exists $opts->{d});
         next;
     }
 
@@ -95,36 +112,41 @@ while (<EXEC>) {
     }
 
      next
-         if ((exists $opts->{u} || exists $opts->{f}) && ! grep /^$pidm_map{$pidm}/, @users2import);    
+         if ((exists $opts->{u} || exists $opts->{f}) && ! grep /^$pidm_map{$pidm}$/i, @users2import);
+
+    next
+        if (defined $name && exists $opts->{c});
 
     if (!defined $pidm || $pidm =~ /^\s*$/) {
         print "ignoring calender with no owner: /$_/\n";
     }
 
-    print "/$_/\n";
+    #print "/$_/\n";
 
     my $ics_cmd = "./csexport -c $pidm";
     if (defined $name && $name !~ /^\s*$/) {
         $ics_cmd .= ":$name";
     }
     $ics_cmd .= " calendar ${ics_out_dir}/$pidm_map{$pidm}";
-    $ics_cmd .= "_$name"
+    $ics_cmd .= ":$name"
         if (defined $name && $name !~ /^\s*$/);
     $ics_cmd .= ".ics\n";
     
-    print "$ics_cmd";
+    print  "/$_/ $ics_cmd";
     system "cd $cal_bin && $ics_cmd";
 }
 
 
 sub print_usage() {
 
-    print "\nusage: $0 -p <pidm map> -o <ics output dir>\n".
-        "\t[ -u <user_list> || -f <user list file>]\n";
+    print "\nusage: $0 [-c] -p <pidm map> -o <ics output dir>\n".
+        "\t[ -u <user_list> || -f <user list file> || -a]\n";
     print "\n";
+    print "\t-c only export primary calendars\n";
     print "\t-p <pidm map> csv containing pidm to username map\n";    
     print "\t-o <ics output dir> directory to put exported ics files\n";
-    print "\t-u <user_list> whitespace separated list of users to import\n";
-    print "\t-f <user list file> <cr> separated  list of users to import\n";
+    print "\t-u <user_list> whitespace separated list of users to export\n";
+    print "\t-f <user list file> <cr> separated  list of users to export\n";
+    print "\t-a export all calendars\n";
     exit;
 }
