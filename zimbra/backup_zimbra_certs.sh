3d6e6f414b6920470c7d530edce9689d4a0a6e57
Morgan Jones <morgan@morganjones.org>
2009-08-24 16:46:07 +0000

    added description, instructions, etc. removed sdp-specific values from site-specific settings

--- zimbra/backup_zimbra_certs.sh
+++ zimbra/backup_zimbra_certs.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 #
+# Morgan Jones (morgan@morganjones.org)
+# $Id$
+# Description: login to indicated Zimbra hosts and backup all ssl certs.
+#    Designed to be used prior to installing new certs on a Zimbra infrastructure in case you need
+#    to revert to the old certs.  Zimbra saves certs in so many different places it can be daunting 
+#    and error prone to back them all up.  I suggested that you establish password-less ssh with keys
+#    before running this.  Otherwise you will be prompted for a password over and over during the backup.
+# Instructions: list all your hosts in the 'hosts' variable, 'domain' will be appended to each of $hosts.
+#    This will create a directory for each host in the current directory.  If the directory already 
+#    exists it will complain and not save anything in it.
+#
 
-domain="domain.org"
-# hosts="dmldap01 \
-# dmta01 \
-# dmail01 dmail02"
 
-hosts="mldap01 mldap02 \
+### site-specific settings
+domain="morganjones.org"
+hosts="ldap01 ldap02 \
 mta01 mta02 mta03 \
-mail01 mail02 mail03 mail04 mail05 mail06 mail07"
+store01 store02 store03 store04 store05 store06 store07"
 
+
+### You shouldn't need to change anything below here.
 zimbra_home="/opt/zimbra"
-
 files="\
 ssl/zimbra/commercial/commercial.key ssl/zimbra/commercial/commercial.crt ssl/zimbra/commercial/commercial_ca.key \
 conf/smtpd.crt conf/smtpd.key \
