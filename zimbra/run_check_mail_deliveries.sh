5b02a514e7d939b697a5963a559fa42b79d95b0b
Morgan Jones <morgan@morganjones.org>
2010-04-06 16:21:43 +0000

    initial version

---
+++ zimbra/run_check_mail_deliveries.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+
+log_base="/home/morgan/logs_for_dev"
+log_file=`ls -1trah ${log_base}/????.??.??/dsmdc-mail-bxga3/mail.log|tail -1`
+output="/usr/local/lib/check_mail_deliveries.out"
+
+/usr/local/nagios/libexec/check_mail_deliveries.pl -p 20 -f ${log_file} > \
+    ${output}
