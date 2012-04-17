f4032a56cf77d6c3e40d96a1f121af003ef2d2af
Morgan Jones <morgan@morganjones.org>
2012-04-17 21:18:18 +0000

    cleanup, no functional changes

--- zimbra/build_one_ogc.sh
+++ zimbra/build_one_ogc.sh
@@ -6,49 +6,30 @@ if [ `whoami` != "root" ]; then
     exit
 fi
 
+ogc_num=15
+
 u=$1
 echo $u
 
 echo "starting at `date`"
+echo; echo "working on $u: "
+archive=`sudo su - zimbra -c "zmprov ga $u zimbraarchiveaccount|grep -i archive"|awk '{print $2}'`
+last_date=`sudo su - zimbra -c "/usr/local/bin/last_message_date.pl -m $archive"`
+echo $last_date
 
-#rm -rf /var/tmp/xmbox_ogc14_*
+dir=/var/mail_log/discover/xmbox_ogc${ogc_num}_$u
+if [ -d $dir ]; then
+    echo "$dir exists! remove it first!"
+    exit
+fi
+mkdir $dir
 
-# echo "checking for directories in /var/tmp..."
-# for u in $users; do
-#     dir=/var/tmp/xmbox_ogc${ogc_num}_$u
-#     if [ -d $dir ]; then
-# 	echo "$dir exists! remove it first!"
-# 	exit
-#     fi
-# done
-
-# echo -n "finding ogc number: "
-# ogc_num=`su - zimbra -c "zmprov -l gaa|egrep '^_ogc'"|awk -F_ '{print $2}'|sed 's/ogc//'|sort -un|tail -1`
-# ogc_num=`expr $ogc_num + 1`
-# echo "ogc number: $ogc_num"
-ogc_num=15
-
-#for u in $users; do
-    echo; echo "working on $u: "
-    archive=`sudo su - zimbra -c "zmprov ga $u zimbraarchiveaccount|grep -i archive"|awk '{print $2}'`
-#    echo -n "$archive "
-    last_date=`sudo su - zimbra -c "/usr/local/bin/last_message_date.pl -m $archive"`
-    echo $last_date
-
-    dir=/var/mail_log/discover/xmbox_ogc${ogc_num}_$u
-    if [ -d $dir ]; then
- 	echo "$dir exists! remove it first!"
- 	exit
-    fi
-    mkdir $dir
-
-    cmd="/usr/local/zmmboxsearchx-20100625/bin/zmmboxsearchx --query in:/Inbox --limit 0 --dir $dir --account $archive"
-    echo $cmd
-    $cmd
-    cmd2="/usr/local/zmmboxsearchx-20100625/bin/zmmboxsearchx --query "before:${last_date}" --limit 0 --dir $dir --account $u"
-    echo $cmd2
-    $cmd2
-#done
+cmd="/usr/local/zmmboxsearchx-20100625/bin/zmmboxsearchx --query in:/Inbox --limit 0 --dir $dir --account $archive"
+echo $cmd
+$cmd
+cmd2="/usr/local/zmmboxsearchx-20100625/bin/zmmboxsearchx --query "before:${last_date}" --limit 0 --dir $dir --account $u"
+echo $cmd2
+$cmd2
 
 echo
 echo "finished at `date`"
