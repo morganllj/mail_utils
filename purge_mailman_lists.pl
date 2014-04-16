db1b3d781cf8551240b115eda387e08c3487bd3b
morgan jones <morgan@morganjones.org>
2014-04-16 16:24:58 -0400

    initial version, working in production but limited in configurability

---
+++ purge_mailman_lists.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl -w
+#
+
+use Getopt::Std;
+
+my %opts;
+getopt ('n', \%opts);
+
+print "-n used, no changes will be made\n\n"
+  if (exists $opts{n});
+
+my $archive_path = qw:/var/lib/mailman/archives:;
+my $mbox_purge = qw:/usr/local/bin/mbox-purge:;
+my $arch = qw:/usr/lib/mailman/bin/arch:;
+print "archive_path: $archive_path\n";
+
+my $t = time();
+
+# 86400 seconds in a day
+my $days_ago = time() - (86400 * 30);
+
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = 
+  localtime ($days_ago);
+
+$year +=1900;
+$mon++;
+$mon = "0" . $mon
+  if ($mon < 10);
+$mday = "0" . $mday
+  if ($mday < 10);
+
+
+#my $archives = `ls /var/mail_log/archives/*/*mbox/*mbox`;
+#chomp $archives;
+my @archives = split /\n/, `ls $archive_path/*/*mbox/*mbox`;
+for my $a (@archives) {
+    print "\n$mbox_purge --before $year-$mon-$mday $a\n";
+    unless (exists $opts{n}) {
+	die "problem with mbox-purge, aborting."
+	  if (system "$mbox_purge --before $year-$mon-$mday $a\n");
+    }
+
+    # the mbox is deleted if there are no messages in it
+    if (-e $a) {
+	my @l = split /\//, $a;
+	$listname = $l[-1];
+      
+	$listname =~ s/.mbox$//;
+	print "$arch --wipe $listname\n";
+	unless (exists $opts{n}) {
+
+	    die "problem with $arch, aborting."
+	      if (system "$arch --wipe $listname");
+	}
+    }
+}
+
