b6512e80839b2050faeea0f7266a82ac75dcd0d5
morgan jones <morgan@morganjones.org>
2016-02-24 14:21:29 -0500

    moved out of zimbra subfolder

---
+++ check_mail_sent.pl
@@ -0,0 +1,166 @@
+#!/usr/bin/perl -w
+#
+# name: check_mail_sent.pl
+#
+# description: parse a postfix log file and report on an email
+#     address(es) that send more than the selected quantity of mail.
+# author: Morgan Jones (morgan@morganjones.org)
+# date: 11/28/12
+#
+# Unless your log is very small and assuming you use nrpe you will
+# need to increase your nrpe timeout.  On a 750mb log file with a 60
+# min period the plugin takes around 20 secs:
+# vi commands.cfg
+#    $USER1$/check_nrpe -H $HOSTADDRESS$ -c $ARG1$ -t 60
+
+use strict;
+use Getopt::Std;
+require "timelocal.pl";
+
+sub print_usage();
+sub get_concise_time($);
+
+
+my %opts;
+getopts('f:p:w:c:di:e:t', \%opts);
+
+my $filename = $opts{f} || print_usage();
+my $time_period = $opts{p} || print_usage(); # start parsing $time_period 
+    # minutes before the date of the last line line the log file.
+my $warn_level = $opts{w} || print_usage();
+my $critical_level = $opts{c} || print_usage();
+
+if (exists $opts{e}) {
+    print "-e not yet implemented.\n";
+    exit (1);
+}
+
+print "-d used, printing debugging..\n\n"
+  if (exists $opts{d});
+
+my @include_domains;
+if (exists $opts{i}) {
+    @include_domains = split /\s*,\s*/, $opts{i};
+    print "only alerting for these domains: ", join " ", @include_domains, "\n"
+      if ($opts{d});
+}
+
+my %mon2num = qw( Jan 0  Feb 1  Mar 2  Apr 3  May 4  Jun 5 Jul 6 Aug 7 Sep 8 
+                  Oct 9 Nov 10 Dec 11 );
+
+# get the last line of the log to know the time at the end of the log file
+my $last_line = `tail -1 $filename`;
+
+# format we're parsing: Mar 16 23:59:59
+my ($mon, $day, $hour, $min, $sec) = 
+    ($last_line =~ /([a-z]{3})\s+(\d{1,2})\s(\d{2}):(\d{2}):(\d{2})/i);
+my $year = (localtime(time()))[5] + 1900;
+my $end_time = timelocal($sec, $min, $hour, $day, $mon2num{$mon}, $year);
+
+my $start_time;  # time we start parsing log entries: $end_time - ($time_period * 60)
+
+open IN, $filename || die "can't open $filename";
+
+my %addrs;
+my $printed_first = 0;
+
+while (<IN>) {
+	if (/postfix\// && /from=<([^>]+)>,/) {
+	    my $addr = $1;
+	    chomp;
+
+	    my ($l_mon, $l_day, $l_hour, $l_min, $l_sec) = 
+	      /([a-z]{3})\s+(\d{1,2})\s(\d{2}):(\d{2}):(\d{2})/i;
+            next
+#              if (!defined $l_day);
+	    if (!defined $l_mon || !defined $l_day || !defined $l_hour || !defined $l_min || !defined $l_sec);
+	    my $line_time =
+	      timelocal($l_sec, $l_min, $l_hour, $l_day, $mon2num{$l_mon}, $year);
+	    next unless ($line_time > ($end_time - ($time_period * 60)));
+
+	    if (!$printed_first && exists $opts{d}) {
+		print "first line: $_\n";
+		print "last line: $last_line\n";
+		$printed_first = 1;
+	    }
+
+	    my $addr_domain = (split (/\@/, $addr))[-1];
+
+	    if (!exists $opts{i} || grep /\Q$addr_domain\E/i, @include_domains) {
+		if (exists $addrs{lc $addr}) {
+		    $addrs{lc $addr}++;
+		} else {
+		    $addrs{lc $addr} = 1;
+		}
+	    }
+	}
+}
+
+
+my $rc=0;
+my @status;
+
+for my $addr (sort keys %addrs) {
+    push @status, $addr . " " . $addrs{$addr}
+      if ($addrs{$addr} >= $warn_level || $addrs{$addr} >= $critical_level);
+    $rc = 1
+      if (($addrs{$addr} >= $warn_level) && ($rc < 1) );
+    $rc = 2
+      if (($addrs{$addr} >= $critical_level) && ($rc < 2) );
+}
+
+if ($rc == 0) {
+    print "OK";
+} elsif ($rc == 1) {
+    print "WARNING ";
+} elsif ($rc == 2) {
+    print "CRITICAL ";
+} else {
+    print "UNKNOWN ";
+}
+
+print join (' ', @status);
+
+if (exists $opts{t}) {
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = gmtime (time);
+
+    $year += 1900;
+
+    $sec = 0 . $sec if ($sec<10);
+    $min = 0 . $min if ($min<10);
+    $hour = 0 . $hour if ($hour<10);
+    $mon++;
+    $mon = 0 . $mon if ($mon<10);
+
+    my $timestamp = join '', ($year,$mon,$mday,$hour,$min,$sec,"Z");
+    print " " . $timestamp;
+}
+
+print "\n";
+exit $rc;
+
+
+######
+sub print_usage() {
+    print "usage:\n";
+    print "$0 [-d] -p <time period> -w <warn level> -c <critical level> -f <filename>\n";
+    print "\t[-e domain1,domain2,... -i domain1,domain2,...]\n";
+    print "\n";
+    print "[-d] print debug output, optional\n";
+    print "-p <time period> look back this many minutes in the log file\n";
+    print "-f <filename> log filename to open\n";
+    print "[-w <level>] warn level\n";
+    print "[-c <level>] critical level\n";
+    print "[-e domain1,domain2,... -i domain1,domain2,...] exclude (-e) or include (-i)\n";
+    print "\tdomains from/for alarming.\n";
+    print "\t-e is not implemented.\n";
+    print "[-t] print timestamp.  Used when outputting to a file from cron to ensure it's unique.\n";
+    print "\tWe then do a checksum in Zabbix and if it doesn't change we know cron isn't running\n";
+    print "\tthe script.\n";
+    
+    print "\n";
+
+    exit;
+}
+
+
