e26cb02d99c4d020bebf38271418027e0169d40f
morgan jones <morgan@morganjones.org>
2016-06-23 16:19:02 -0400

    implemented -e to exclude based on an arbitrary expression

--- check_mail_sent.pl
+++ check_mail_sent.pl
@@ -22,6 +22,7 @@ require "timelocal.pl";
 
 sub print_usage();
 sub get_concise_time($);
+sub excluded($);
 
 
 my %opts;
@@ -33,9 +34,14 @@ my $time_period = $opts{p} || print_usage(); # start parsing $time_period
 my $warn_level = $opts{w} || print_usage();
 my $critical_level = $opts{c} || print_usage();
 
+my @exclude;
 if (exists $opts{e}) {
-    print "-e not yet implemented.\n";
-    exit (1);
+    # print "-e not yet implemented.\n";
+    # exit (1);
+
+    @exclude = split /\s*,\s*/, $opts{e};
+    print "-e used, excluding these domains/addresses: ", join " ", @exclude, "\n\n"
+      if ($opts{d});
 }
 
 print "-d used, printing debugging...\n\n"
@@ -93,28 +99,29 @@ my %addrs;
 my $printed_first = 0;
 
 while (<IN>) {
-	if (/postfix\// && /from=<([^>]+)>,/) {
-	    my $addr = $1;
-	    chomp;
+    if (/postfix\// && /from=<([^>]+)>,/) {
+	my $addr = $1;
+	chomp;
 
-	    my ($l_mon, $l_day, $l_hour, $l_min, $l_sec) = 
-	      /([a-z]{3})\s+(\d{1,2})\s(\d{2}):(\d{2}):(\d{2})/i;
-            next
-#              if (!defined $l_day);
-	      if (!defined $l_mon || !defined $l_day || !defined $l_hour || !defined $l_min || !defined $l_sec);
-	    my $line_time =
-	      timelocal($l_sec, $l_min, $l_hour, $l_day, $mon2num{$l_mon}, $year);
-	    next unless ($line_time > ($end_time - ($time_period * 60)));
+	my ($l_mon, $l_day, $l_hour, $l_min, $l_sec) = 
+	  /([a-z]{3})\s+(\d{1,2})\s(\d{2}):(\d{2}):(\d{2})/i;
+	next
+	  if (!defined $l_mon || !defined $l_day || !defined $l_hour || !defined $l_min || !defined $l_sec);
+	my $line_time =
+	  timelocal($l_sec, $l_min, $l_hour, $l_day, $mon2num{$l_mon}, $year);
+	next unless ($line_time > ($end_time - ($time_period * 60)));
 
-	    if (!$printed_first && exists $opts{d}) {
-		print "first line: $_\n\n";
-#		print "last line: $last_line\n";
-		$printed_first = 1;
-	    }
+	if (!$printed_first && exists $opts{d}) {
+	    print "first line: $_\n\n";
+	    $printed_first = 1;
+	}
 
-	    my $addr_domain = (split (/\@/, $addr))[-1];
+	my $addr_domain = (split (/\@/, $addr))[-1];
 
-	    if (!exists $opts{i} || grep /\Q$addr_domain\E/i, @include_domains) {
+#	if (!exists $opts{i} || grep /\Q$addr_domain\E/i, @include_domains) {
+	if (exists $opts{i} && grep /\Q$addr_domain\E/i, @include_domains) {
+
+	    unless (excluded($addr)) {
 		if (exists $addrs{lc $addr}) {
 		    $addrs{lc $addr}++;
 		} else {
@@ -122,7 +129,30 @@ while (<IN>) {
 		}
 	    }
 	}
+    }
 }
+
+
+{
+    my %excluded_addrs;
+
+    sub excluded ($) {
+	my $in_addr = shift;
+
+	return 1 if (exists $excluded_addrs{$in_addr});
+	
+	for my $e (@exclude) {
+	    if ($in_addr =~ /$e/) {
+		$excluded_addrs{$in_addr} = 1;
+		return 1;
+	    }
+	}
+	
+	return 0;
+    }
+}
+
+
 
 
 my $rc=0;
@@ -130,7 +160,6 @@ my @status;
 
 for my $addr (sort keys %addrs) {
     if ($addrs{$addr} >= $warn_level || $addrs{$addr} >= $critical_level) {
-	#    push @status, $addr . " " . $addrs{$addr}
 	push @status, $addr;
 	push @status, $addrs{$addr};
     }
@@ -284,7 +313,8 @@ sub print_usage() {
     print "-f <filename> log filename to open\n";
     print "[-w <level>] warn level\n";
     print "[-c <level>] critical level\n";
-    print "[-e domain1,domain2,... -i domain1,domain2,...] exclude (-e) or include (-i)\n";
+    print "[-e <expression> -i domain1,domain2,...] exclude (-e) or include (-i)\n";
+    print "\t<expression> can any part of an address: sender\@domain.com, domain.com, sender, ender, .com, etc\n";
     print "\tdomains from/for alarming.\n";
     print "\t-e is not implemented.\n";
     print "[-t] print timestamp.  Used when outputting to a file from cron to ensure it's unique.\n";
