6f7be4bce04505809287c66944b47babbd4c47e3
Morgan Jones <morgan@larry-2.local>
2016-11-09 18:10:36 -0500

    now searches all users, all archives or both various big fixes--didn't search archives correctly prior

--- zimbra/search_all_mboxes.sh
+++ zimbra/search_all_mboxes.sh
@@ -6,6 +6,16 @@
 
 #eval set -- "$OPTS"
 
+
+print_usage() {
+    echo "usage: $0 -s 'user search str'|| -a 'archive search str' -o <output>"
+    echo -e "\tyou must specify -s, -a or both"
+    echo
+    exit
+}
+
+
+
 while getopts ":s:a:o:" opt; do
     case "$opt" in
 	s) usrsrch=$OPTARG;;
@@ -18,10 +28,12 @@ echo "s: $usrsrch"
 echo "a: $arcsrch"
 echo "o: $out"
 
-if [ 'x' == "x$usrsrch" ] || [ 'x' == "x$out" ]; then
-    echo "usage: $0 -s 'user search str' [ -a 'archive search str' ] -o <output>"
-    echo
-    exit
+if [ "X" == "x$usrsrch" ] || [ "x" == "x$arcsrch" ]; then
+    print_usage
+fi
+
+if [ "x" == "x$out" ]; then
+    print_usage
 fi
 
 if [ ! -d $out ]; then
@@ -31,17 +43,21 @@ fi
 
 echo starting at `date` | tee -a ${out}.out
 
-for acct in `zmprov -l gaa|grep -v archive|sort -n`; do
-  echo -n acct: $acct " " | tee -a ${out}.out
-  zmmboxsearch -m $acct -d ${out} -q $usrsrch -p 10000000 2>&1 | tee -a ${out}.out
-done
+if [ 'x' != "x$usrsrch" ]; then
+    for acct in `zmprov -l gaa|grep -v archive|sort -n`; do
+	echo -n acct: $acct " " | tee -a ${out}.out
+	zmmboxsearch -m $acct -d ${out} -q "$usrsrch" -p 10000000 2>&1 | tee -a ${out}.out
+    done
+fi
 
-if [ 'x' == "x$arcsrch" ]; then
+if [ 'x' != "x$arcsrch" ]; then
     for acct in `zmprov -l gaa|grep archive|sort -n`; do
 	echo -n acct: $acct " " | tee -a ${out}.out
-	zmmboxsearch -m $acct -d ${out} -q $arcsrch -p 10000000 2>&1 | tee -a ${out}.out
+	zmmboxsearch -m $acct -d ${out} -q "$arcsrch" -p 10000000 2>&1 | tee -a ${out}.out
     done
 fi
 
 echo finished at `date` | tee -a ${out}.out
+
+
 
