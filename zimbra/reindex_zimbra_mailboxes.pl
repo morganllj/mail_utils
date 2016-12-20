4a4f37e91d803426a434ad94754d47c1a01616ac
Morgan Jones <morgan@morganjones.org>
2016-12-20 14:04:49 -0500

    cleaned up output, better checks status, shouldn't loop forever on an error it doesn't recognize

--- zimbra/reindex_zimbra_mailboxes.pl
+++ zimbra/reindex_zimbra_mailboxes.pl
@@ -37,26 +37,33 @@ sub reindex {
 	print "\nreindexing $a at ", `date`;
 
 	my $cmd = "zmprov rim $a start 2>&1";
-	print "${a}) ${cmd}\n";
+	print "${a} ${i}) ${cmd}\n";
 
 	if (!defined $opts{n}) {
 	    my $out = `$cmd`;
 	    chomp $out;
-
-	    print "${a}) $out\n";
+	    print "${a} ${i}) $out\n";
 
 	    my $statuscmd = "zmprov rim $a status 2>&1";
 	    $out = `$statuscmd`;
 
-	    while ($out !~ /status: idle/) {
+	    my $c=0;
+	    while ($out =~ /progress:/) {
 		sleep 5;
 		
 		$out = `$statuscmd`;
-		#		print "${a}) ${statuscmd}\n";
-		chomp $out;
 		$out =~ s/status: running\n//;
-		print "${a}) $out\n";
+		chomp $out;
+
+		# print roughly every minute.  Count of 7 compensates
+		# for time time it takes to run zmprov rim status.
+		if ($c++==7) {
+		    print "${a} ${i}) $out\n";
+		    $c=0;
+		}
 	    }
+	    print "${a} ${i}) $out\n"
+	      if ($i>1);
 
 	}
 	$i++;
@@ -67,6 +74,6 @@ sub reindex {
 
 
 sub print_usage {
-    print "usage: $0 -n -a | -u user1,user2,...\n\n";
+    print "usage: $0 [-n] -a | -u user1,user2,...\n\n";
     exit;
 }
