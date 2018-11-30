aef6037bb4be67f2cd4c811d638aea70564b33eb
Morgan Jones <morgan@morganjones.org>
2018-11-30 16:38:54 -0500

    in process, not finished

--- zimbra/find_zimbra_db_files.py
+++ zimbra/find_zimbra_db_files.py
@@ -9,7 +9,22 @@ import re
 
 #print help(subprocess)
 
-output=subprocess.Popen("zmlocalconfig -s", shell=True, stdout=subprocess.PIPE)
+# output=subprocess.Popen("zmlocalconfig -s", shell=True, stdout=subprocess.PIPE)
+# out,err = output.communicate();
+
+# for l in out.split("\n"):
+#     if re.match(r"\w+\s*=\s*\w", l): 
+#         k,v = re.compile("\s+=\s+").split(l)
+#         p = "/"+k+"/ /"+v+"/"
+#         if k == "zimbra_mysql_user":
+#             mysql_user = v
+#         elif k == "zimbra_mysql_password":
+#             mysql_pass = v
+
+# print mysql_user, mysql_pass
+
+
+output=subprocess.Popen("mysql -e "select comment from mailbox", shell=True, stdout=subprocess.PIPE)
 out,err = output.communicate();
 
 for l in out.split("\n"):
@@ -22,4 +37,3 @@ for l in out.split("\n"):
             mysql_pass = v
 
 print mysql_user, mysql_pass
-
