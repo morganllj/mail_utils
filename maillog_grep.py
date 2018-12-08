573fc7e7c78c44a7584d23a3ad60eb435a0be731
Morgan Jones <morgan@morganjones.org>
2018-12-08 14:13:15 -0500

    provisionally working, need to de-dup entries

--- maillog_grep.py
+++ maillog_grep.py
@@ -33,49 +33,8 @@ def in_qids_to_print(q):
         return 1
     return 0
 
-# def check_qids(q,a,r,ft,l):
-#     qid  = q
-#     in_addr = a
-#     r_obj = r
-#     in_fmto = ft
-#     in_line = l
 
-#     if in_addr is not None:
-#         # the user passed both from and to: look for the complementing from/to and print it if so
-#         if qid in qids.keys():
-#             matched = 0
-#             for v in qids[qid]:
-#                  mo = re.search(r_obj, in_line)
-#                  qid = mo.group(1)
-#                  fmto = mo.group(2)
-#                  addr = mo.group(3)
-#                  if addr.lower() == in_addr.lower() and fmto.lower() == in_fmto.lower():
-#                      matched = 1
-#             if matched:
-#                 # cycle through, print, and delete from qids
-#                 for v in qids[qid]:
-#                     print (v)
-#                     del qids[qid]
-#                 # and print the current line
-#                 print (in_line)
-#                 # print all future occurrences of this qid
-#                 #qids_to_print.append(qid)
-#                 add_to_qids_to_print(qid)
-#                 # tell the caller we printed
-#                 return 1
-#     else:
-#         # the user only passed from or to, print everything we have for the qid so far
-#         if qid in qids.keys():
-#             for v in qids[qid]:
-#                 print (v)
-#                 del qids[k]
-#         print ("check_qids, else: "+in_line)
-
-# #        qids_to_print.append(qid)
-#         add_to_qids_to_print(qid)
-#         return 1
-#     return 0
-
+####### main
 opts, args = getopt.getopt(sys.argv[1:], "f:t:m:")
 
 for opt, arg in opts:
@@ -93,12 +52,6 @@ if file is None:
 
 if fm is None and to is None:
    print_usage()
-        
-print ("file: "+file)
-if to is not None:
-    print ("to: "+to)
-if fm is not None:
-    print ("from: "+fm)
 
 # this is too specific if I want all log lines, it will get just froms and tos which might be enough.
 r_obj = re.compile(r'mta\d\d postfix[^:]+: ([^:]+): (from|to)=<([^>]+)>')
@@ -115,55 +68,37 @@ for line in open(file):
         fmto = mo.group(2)
         addr = mo.group(3)
 
-#        print ("\n"+qid, fmto, addr)
-
+        # all lines go in qids, qid as index
         if qid not in qids.keys():
             qids[qid] = []
-        print ("a:",qid, line)
         qids[qid].append(line)
         
         if ((fmto.lower() == "from" and fm is not None and fm.lower() == addr.lower()) or
             (fmto.lower() == "to" and to is not None and to.lower() == addr.lower())):
+            # The current line matches either from or two provided on the cli
+            
             if ((fmto.lower() == "from" and to is not None) or
                 (fmto.lower() == "to"   and fm is not None)):
-
-                # duplicated from below!
-                if in_qids_to_print(qid):
-                    for l in qids[qid]:
-                        print (l)
-                    del qids[qid]
-                else: # not in qids_to_print, decide if this one is both from and to the right addrs
-                    matched = 0
-                    for l in qids[qid]:
-                        mo2 = re.search(r_obj, l)
-                        if mo2:
-                            fmto2 = mo2.group(2)
-                            addr2 = mo2.group(3)
-                            if fmto.lower() == "from" and fmto2.lower() == "to":
-                                print ("comparing", to.lower(), addr2.lower())
-                                if to.lower() == addr2.lower():
-                                    matched = 1
-                            elif fmto.lower() == "to" and fmto2.lower() == "from":
-                                if to.lower() == addr2.lower():
-                                    matched = 1
-                    if matched:
-                        # duplicate!
-                        add_to_qids_to_print(qid)
-                        for l in qids[qid]:
-                            print (l)
-                        del qids[qid]
-
+                # both a front and to were provided on the cli
+                # cycle through qids[qid], if both a from and to match print what's there add qid to qids_to_print
+                for l in qids[qid]:
+                     mo2 = re.search(r_obj, l)
+                     if mo2:
+                         fmto2 = mo2.group(2)
+                         addr2 = mo2.group(3)
+                         if ((fmto.lower() == "from" and fmto2.lower() == "to"   and to.lower() == addr2.lower()) or
+                             (fmto.lower() == "to"   and fmto2.lower() == "from" and fm.lower() == addr2.lower())):
+                            # we have a matching from/to combo: print it!
+                            add_to_qids_to_print(qid)
             else:
+                # only a from or to was specified on the cli: print it!
                 add_to_qids_to_print(qid)
-                for l in qids[qid]:
-                    print (l)
-                del qids[qid]
-        else:
-            # there's no match but if the qid is one we need to print, print it
-            if in_qids_to_print(qid):
-                for l in qids[qid]:
-                    print (l)
-                del qids[qid]
+
+        # catch-all: if qid is in qids_to_print, print everything we have for it and remove it from qids
+        if in_qids_to_print(qid):
+            for l in qids[qid]:
+                print (l)
+            del qids[qid]
                 
         
 
