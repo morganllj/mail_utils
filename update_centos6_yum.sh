4f572db3314ab96feec9d07b8cfba735c365cdef
Morgan Jones <morgan@morganjones.org>
2021-08-22 17:11:42 -0400

    update to automatically find the mirrorlist location.  I'm not sure it's working.

--- update_centos6_yum.sh
+++ update_centos6_yum.sh
@@ -1,7 +1,17 @@
 #! /bin/bash
 
-echo "https://vault.centos.org/6.10/" >> /var/cache/yum/base/mirrorlist.txt
-echo "https://vault.centos.org/6.10/" >> /var/cache/yum/extras/mirrorlist.txt
-echo "https://vault.centos.org/6.10/" >> /var/cache/yum/updates/mirrorlist.txt
+dir1=/var/cache/yum/x86_64/6
+dir2=/var/cache/yum
+
+if [ -d $dir1 ]; then
+  dir=$dir1
+else
+  dir=$dir2
+fi 
+
+for i in base extras updates; do 
+  echo ${dir}/$i/mirrorlist.txt
+  echo "https://vault.centos.org/6.10/" >> /var/cache/yum/x86_64/6/$i/mirrorlist.txt
+done
 
 sed -i -e 's|^#*baseurl.*$|baseurl=http://vault.centos.org/6.10/centosplus/$basearch/|g' -e 's/^mirrorlist/#mirrorlist/g' /etc/yum.repos.d/Cent*-Base.repo
