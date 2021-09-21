dd10f824909908628255f7b464ab4b4d5fa2a4bc
Morgan Jones <morgan@morganjones.org>
2021-09-21 15:16:36 -0400

    added uptime to queue check

--- mta_queue_check.sh
+++ mta_queue_check.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
 
-while [ 1 ]; do for i in 4 5 6; do echo; echo mta0${i}; ssh mta0${i}.philasd.org "sudo su -c /opt/zimbra/libexec/zmqstat"; sleep 3; done; done
+while [ 1 ]; do for i in 4 5 6; do echo; echo mta0${i}; ssh mta0${i}.philasd.org "uptime; sudo su -c /opt/zimbra/libexec/zmqstat"; sleep 3; done; done
